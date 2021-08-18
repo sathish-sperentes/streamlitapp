071b8d3985c816223896117d553fcc86320f13eb
Sathish Kumar <62051056+sathish-sperentes@users.noreply.github.com>
2021-08-18 16:59:36 +0530

    Add files via upload

---
+++ first_app.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+st.title("My First App")
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+'''
+# Another way of getting the above output
+# My First App
+Here's our first attempt at using data to create a table:
+'''
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4, 5],
+    'second column': [10, 20, 30, 40, 50]
+})
+
+df
+
+'''
+# Let's create a line chart
+'''
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+'''
+# Plotting a map is so easy
+'''
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+'''
+# Using widgets
+# Use a checkbox
+'''
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+
+    chart_data
+
+'''
+# Use a select box
+
+option = st.selectbox(
+    'Which number do you like best?',
+    df['first column'])
+
+'You selected: ', option
+'''
+
+'''
+# Use a sidebar
+'''
+option2 = st.sidebar.selectbox(
+    'Which number do you like best?',
+    df['first column'])
+
+'You selected: ', option2
+
+'''
+#  Add more options to the sidebar widget
+'''
+left_column, right_column = st.columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+    right_column.write("Whoohoo!")
+
+expander = st.expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
+
+'''
+# Show progress bar
+'''
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    # Update the progress bar with each iteration
+    latest_iteration.text(f'Iteration (i+1)')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'...and now we\'re done!'
